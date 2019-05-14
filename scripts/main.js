de7b98d232cd4378c2ee0ef860791178bf861e0e
Eli Zhang <zhang.eli@hotmail.com>
2019-05-14 15:04:29 -0400

    added while loop to handle results that not videos

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,12 @@ let wordInput = '';
 let URL ='';
 
 function getVideoId(object) {
-    const vidId = object.items[0].id.videoId;
+    let count = 0;
+    let vidId = object.items[count].id.videoId;
+    while (vidId === undefined) {
+        count++;
+        vidId = object.items[count].id.videoId;
+    }
     const iFrame = videoDiv.getElementsByTagName("iframe")[0];
     iFrame.setAttribute("src", `https://www.youtube.com/embed/${vidId}`);
 }
@@ -15,7 +20,7 @@ searchInput.addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
     if (key === 13) {
         //Updates empty string with the inputted search term from search bar
-        wordInput += searchInput.value;
+        wordInput += searchInput.value.split(' ').join('+');
         //Updates the query search with the inputted search term from search bar
         URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${wordInput}&key=AIzaSyB9WzlCfQKAWzLTqAsrcepelEEUT4b8NPk`;
         updatePage();
