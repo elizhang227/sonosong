59fb9616b73c13b60766478544cb291e61146f12
Eli Zhang <zhang.eli@hotmail.com>
2019-05-14 14:29:08 -0400

    refactored some code and deleted unneccessary lines

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,17 @@ let URL ='';
 
 function getVideoId(object) {
     const vidId = object.items[0].id.videoId;
-    //console.log(vidId);
     const iFrame = videoDiv.getElementsByTagName("iframe")[0];
     iFrame.setAttribute("src", `https://www.youtube.com/embed/${vidId}`);
-    console.log(videoDiv.innerHTML);
-
 }
 
 searchInput.addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
     if (key === 13) {
+        //Updates empty string with the inputted search term from search bar
         wordInput += searchInput.value;
+        //Updates the query search with the inputted search term from search bar
         URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${wordInput}&key=AIzaSyB9WzlCfQKAWzLTqAsrcepelEEUT4b8NPk`;
-        console.log(wordInput);
         updatePage();
     }
 })
