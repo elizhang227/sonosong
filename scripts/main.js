5af61e9a91191349584b399d6d248edf50cee113
Eli Zhang <zhang.eli@hotmail.com>
2019-05-14 14:24:41 -0400

    added event listener for search bar and result populates iframe with first video on the list of search results

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
 api_key = "AIzaSyB9WzlCfQKAWzLTqAsrcepelEEUT4b8NPk";
+
+const videoDiv = document.getElementsByClassName('youtubeVideoContainer')[0];
+const searchInput = document.getElementById('searchBarBar');
+let wordInput = '';
+let URL ='';
+
+function getVideoId(object) {
+    const vidId = object.items[0].id.videoId;
+    //console.log(vidId);
+    const iFrame = videoDiv.getElementsByTagName("iframe")[0];
+    iFrame.setAttribute("src", `https://www.youtube.com/embed/${vidId}`);
+    console.log(videoDiv.innerHTML);
+
+}
+
+searchInput.addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        wordInput += searchInput.value;
+        URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${wordInput}&key=AIzaSyB9WzlCfQKAWzLTqAsrcepelEEUT4b8NPk`;
+        console.log(wordInput);
+        updatePage();
+    }
+})
+
+function updatePage() {
+    get(URL)
+    .then((response) =>  {
+        //some functions here
+        getVideoId(response);
+    });
+
+}
+
